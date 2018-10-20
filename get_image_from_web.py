617f43f29215fafc50a172599826954dad5e4bd9
KushnirAndrey <41538907+KushnirAndrey@users.noreply.github.com>
2018-10-20 22:35:59 +0300

    Update get_image_from_web.py

--- get_image_from_web.py
+++ get_image_from_web.py
@@ -2,28 +2,39 @@ import requests
 import bs4
 import re
 
-tag=input('Введите тему картинки на английском-->')
-kol = int(input('Количество картинок->>'))#исключить буквы
-url='''https://www.dreamstime.com/search.php?securitycheck=e0f88b4e731696aec73a2b17b6ccd2f5&
-    firstvalue={}&lastsearchvalue=&srh_field={}&s_ph=y&s_il=y&s_video=y&s_audio=y'''.format(tag,tag)
-#https://picjumbo.com/one-little-angry-cat/ получать картинки с этого сайта
-url = 'https://picjumbo.com/?s={}'.format(tag)
 def open_url(url):
     request = requests.get(url).content
     return bs4.BeautifulSoup(request,features="html5lib")    
 def get_image_url(data, i):
-    img = data.findAll('img')
-    image_url = img[i]
-    return image_url['src'].replace('/t/', '/z/')
+    img = str(data.select('.tri_img_wrap .tri_img_one .tri_img'))
+    img = re.findall(r'src="[\S]+', img)[i] #изменяя [n] Получаем разные картинки
+    img = re.findall(r'"\S+"', img)[0].replace('"', "")
+    print(img)
+    return img
 def save_image(url, i):
     path = '{}.jpg'.format(i)
     img = requests.get(url)
     with open(path, 'wb') as file:
         file.write(img.content)    
 def main():
-    for i in range(kol):
+    tag=input('Введите тему картинки на английском-->')
+    url='https://picjumbo.com/?s={}'.format(tag)
+    while True:
+        try:
+            kol = int(input('Количество картинок->>'))
+            break;
+        except ValueError:
+            print('Введите цифру!')
+    for i in range(1, kol+1):
         save_image(get_image_url(open_url(url), i), i)
 
 if __name__ == '__main__':
-    main()
-
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit
+    except IndexError:
+        print("Слишком много картинок=)")
+        exit
+    except requests.exceptions.MissingSchema:
+        print('По запросу ничего не найдено=(')
